fe5e490313bffc11c4a2530d64d586a9f0751dbb
aaugustomenezes <antonioaugustomenezes@gmail.com>
2022-07-16 13:47:25 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -8,6 +8,7 @@ import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
 function SalesCard() {
+  
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
 
@@ -17,10 +18,17 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+    
+    console.log(dmin);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+        .then((response) => {
+              setSales(response.data.content);
+        });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
